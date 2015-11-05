43e348aac3feadd0b3bc342f59f31befb74e12c4
unknown <dmadner@dmadlaptop.big.tuwien.ac.at>
2015-11-05 19:16:46 +0100

    implement 5min tutorial from angular.io

---
+++ src/app/app.ts
@@ -0,0 +1,7 @@
+import {Component, bootstrap} from 'angular2/angular2';
+@Component({
+    selector: 'dm-alarm-clock',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+class AppComponent { }
+bootstrap(AppComponent);
